2af76a3c2dd1d51bd60b10a72e8958e98408d430
Eveline Sparreboom <eveline.sparreboom@albelli.com>
2021-07-29 17:47:59 +0200

    check for duplicates

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const chai = require( 'chai' );
 const commonProducts = require( '../index' );
 
+const countOccurrences = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+
 describe( 'Common products', () =>
 {
     describe( 'Returns common products from two orders', () =>
@@ -15,6 +17,27 @@ describe( 'Common products', () =>
                 chai.expect(result).to.not.include( product );
             })
         })
+
+    } );
+
+    describe( 'Returns common products from two orders and removes duplicates', () =>
+    {
+        const result = commonProducts( ['mug', 'book', 'card', 'mug'], ['calendar', 'mug', 'card', 'canvas', 'card'] );
+        it('Should contain mug and card', () => {
+            chai.expect(result).to.be.an('array').that.includes.members( ['mug', 'card'] )
+        });
+
+        ['mug', 'card'].forEach( product => {
+            it(`Should not contain more than one ${product}`, () => {
+                chai.expect(countOccurrences(result, product)).to.eq( 1 );
+            })
+        });
+
+        ['book', 'canvas', 'calendar'].forEach( product => {
+            it(`Should not contain ${product}`, () => {
+                chai.expect(result).to.not.include( product );
+            })
+        });
 
     } );
 
